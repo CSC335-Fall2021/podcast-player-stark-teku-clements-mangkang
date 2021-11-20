31a3ec375c01e43ed299db1a3211eb049296888d
Michael Stark <mstark@email.arizona.edu>
2021-11-20 16:43:00 -0700

    Hardcoded podcast feed and ugly play implementation

--- src/PodcastView/PodcastView.java
+++ src/PodcastView/PodcastView.java
@@ -2,23 +2,32 @@ package PodcastView;
 
 
  
+import java.util.ArrayList;
 import java.util.Observable;
 
 import java.util.Observer;
 
 import PodcastController.PodcastController;
+import PodcastEntry.PodcastEpisode;
+import PodcastEntry.PodcastFeed;
 import PodcastModel.PodcastModel;
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -27,17 +36,17 @@ public class PodcastView extends Application implements Observer {
 	private BorderPane obj;
 	private MediaPlayer option;
 	private String URL;
+	private PodcastController controller;
 	
 	@Override
 	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+		PodcastModel model = new PodcastModel();
+		controller = new PodcastController(model);
+		model.addObserver(this);
 		
 		createView();
 		
 		
-		PodcastModel model = new PodcastModel();
-		PodcastController controller = new PodcastController(model);
-		model.addObserver(this);
 		Scene display = new Scene(obj);
 		Stage stage = new Stage();
 		stage.setScene(display);
@@ -54,6 +63,21 @@ public class PodcastView extends Application implements Observer {
 		obj.setPadding(new Insets(8, 8, 8, 8));
 		obj.setStyle("-fx-background-color:#00FF7F; -fx-opacity:1;" );
 		 
+		
+		// Create a list of available podcasts
+		ListView podcastList =new ListView();
+		controller.addPodcastFeed("https://podcastfeeds.nbcnews.com/HL4TzgYC");
+		ArrayList<PodcastFeed> feeds = controller.getPodcastFeeds();
+		ArrayList<PodcastEpisode> episodes = feeds.get(0).getEpisodes();
+		
+		for (PodcastEpisode e : episodes) {
+			podcastList.getItems().add(e.getTitle());
+		}
+	    
+	    Slider timeSlider = new Slider();
+	    timeSlider.setMinWidth(200);
+		
+		VBox player = new VBox(timeSlider, podcastList);
 		
 	 
 		 
@@ -75,7 +99,7 @@ public class PodcastView extends Application implements Observer {
 		
 	     obj.setTop(label);
 		 
-		
+	     obj.setCenter(player);
 		
 		
 		 HBox buttonBar = new HBox( 20, previousTrack,playButton, pauseButton, nextTrack );
@@ -88,9 +112,22 @@ public class PodcastView extends Application implements Observer {
 			 
 			}); 
 		 
-		playButton.setOnMouseClicked((click) -> {
-			// TO BE IMPLEMENTED 
-			option.play();
+		playButton.setOnMouseClicked((click) -> {			
+			for (PodcastEpisode e : episodes) {
+				if (podcastList.getSelectionModel().getSelectedItem().toString().equals(e.getTitle())) {
+					// Stop current playback
+					if (option != null) {
+						option.stop();
+					}
+					
+					// Load and play our new file
+					Media currentMedia = new Media(e.getMediaURL());
+					option = new MediaPlayer(currentMedia);
+					option.setAutoPlay(false);
+					option.play();
+					break;
+				}
+			}			
 		});
 			
 		pauseButton.setOnMouseClicked((click) -> {
